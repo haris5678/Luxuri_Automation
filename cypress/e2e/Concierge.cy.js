071bb7216a8a19188095a642fde0814a73907bf3
harisanwar <harisanwar5678@gmail.com>
2023-12-15 22:39:30 +0500

    Concierge Page test case completed

--- cypress/e2e/Concierge.cy.js
+++ cypress/e2e/Concierge.cy.js
@@ -6,4 +6,23 @@ describe("Villa Page", () => {
     HomePage.Valid_Response();
     conciergePage.concierge_Page();
   });
+  it("It should submit empty form ", () => {
+    HomePage.Valid_Response();
+    conciergePage.Submit_Empty_Inquire_Form();
+  });
+  it("It should submit form without Entering Special Request Field", () => {
+    HomePage.Valid_Response();
+    conciergePage.Form_Without_Special_Request();
+  });
+  it("It should submit form without Entering Prefered Contact Field", () => {
+    HomePage.Valid_Response();
+    conciergePage.Form_Without_Prefered_Contact();
+  });
+  it.only(
+    "It should submit form without Entering Prefered Contact Field",
+    () => {
+      HomePage.Valid_Response();
+      conciergePage.Form_Without_Name();
+    }
+  );
 });
--- cypress/fixtures/ConciergePage.js
+++ cypress/fixtures/ConciergePage.js
@@ -28,14 +28,22 @@ class conciergePage {
       cy.get(".ant-select-item-option-content").eq(0).click({ force: true }),
     Contact_Number: () =>
       cy.get(":nth-child(4) > .ant-input").type("03001231234"),
-    Enter_Name: () => cy.get(":nth-child(5) > .ant-input").type("03001231234"),
+    Enter_Name: () => cy.get(":nth-child(5) > .ant-input").type("test"),
     Press_submit_button: () =>
       cy.get("#conciergeServiceBtn").click({ force: true }),
     Sucess_message: () =>
       cy.get(".swal2-popup").contains("Your request has been submitted"),
-    message_button_ok: () => cy.get(".swal2-confirm").click({ force: true })
+    message_button_ok: () => cy.get(".swal2-confirm").click({ force: true }),
+
+    //negative test cases
+    Error_Message_For_Additional_Request: () =>
+      cy.get(".sc-hmdomO > .sc-aXZVg"),
+    Error_Message_For_prefered_contact_field: () =>
+      cy.get(".sc-fHjqPf > .sc-aXZVg"),
+    Error_Message_For_Name_field: () => cy.get(".sc-hzhJZQ > .sc-aXZVg"),
+    Name_Without_Contact: () => cy.get(".sc-hzhJZQ > .ant-input").type("test")
   };
-  //test
+
   concierge_Page() {
     this.elements.Click_Concierge_Page_Button();
     cy.wait(5000);
@@ -55,6 +63,71 @@ class conciergePage {
     cy.wait(4000);
     this.elements.Sucess_message();
     this.elements.message_button_ok();
+  }
+  Submit_Empty_Inquire_Form() {
+    this.elements.Click_Concierge_Page_Button();
+    cy.wait(5000);
+    this.elements.Click_Massage_therapist_Reserve_Button();
+    cy.scrollTo("bottom");
+    this.elements.Press_submit_button();
+    cy.wait(4000);
+    // Date field error message is missing
+    this.elements
+      .Error_Message_For_Additional_Request()
+      .contains("Request Message is required");
+    this.elements
+      .Error_Message_For_prefered_contact_field()
+      .contains("Contact preference is required");
+
+    this.elements.Error_Message_For_Name_field().contains("Name is required");
+  }
+
+  Form_Without_Special_Request() {
+    this.elements.Click_Concierge_Page_Button();
+    cy.wait(5000);
+    this.elements.Click_Massage_therapist_Reserve_Button();
+    cy.scrollTo("bottom");
+    this.elements.Click_Event_in_form();
+    this.elements.Prefered_contact_method_dropdown();
+    this.elements.Prefered_contact_method_dropdown_first_option();
+    this.elements.Contact_Number();
+    this.elements.Enter_Name();
+    this.elements.Press_submit_button();
+    cy.wait(4000);
+    // Date field error message is missing
+    this.elements
+      .Error_Message_For_Additional_Request()
+      .contains("Request Message is required");
+  }
+
+  Form_Without_Prefered_Contact() {
+    this.elements.Click_Concierge_Page_Button();
+    cy.wait(5000);
+    this.elements.Click_Massage_therapist_Reserve_Button();
+    cy.scrollTo("bottom");
+    this.elements.Click_Event_in_form();
+    this.elements.Special_Request_Field();
+    this.elements.Name_Without_Contact();
+    this.elements.Press_submit_button();
+    cy.wait(4000);
+    // Date field error message is missing
+    this.elements
+      .Error_Message_For_prefered_contact_field()
+      .contains("Contact preference is required");
+  }
+  Form_Without_Name() {
+    this.elements.Click_Concierge_Page_Button();
+    cy.wait(5000);
+    this.elements.Click_Massage_therapist_Reserve_Button();
+    cy.scrollTo("bottom");
+    this.elements.Click_Travel_in_form();
+    this.elements.Click_Event_in_form();
+    this.elements.Special_Request_Field();
+    this.elements.Prefered_contact_method_dropdown();
+    this.elements.Prefered_contact_method_dropdown_first_option();
+    this.elements.Contact_Number();
+    this.elements.Press_submit_button();
+    this.elements.Error_Message_For_Name_field().contains("Name is required");
   }
 }
 
